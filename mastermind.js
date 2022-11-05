f0423a2e4b94bf7e7e622550f7e4530016ef4cfc
SergiBvs <114417148+IkelosVoidz@users.noreply.github.com>
2022-11-05 11:40:55 +0100

    jj
    j

--- mastermind.js
+++ mastermind.js
@@ -1,6 +1,7 @@
 var username;
 var wins, loses;
 var round;
+var randomToGuess;
 
 function startGame(){
     var x = document.getElementById("playerInf");
@@ -9,17 +10,40 @@ function startGame(){
     x.style.display = "none";
     y.style.display = "block";
 
-    var playerName = document.getElementById("fname");
-    username = playerName.value;
-
+    username = document.getElementById("fname").value;
+    
     document.getElementById("playerName").innerHTML = username;
     wins = 0;
     loses = 0;
     round = 1;
+    
+    randomToGuess = randomNumber();
+    console.log(randomToGuess);
+}
+
+function randomNumber(){
+    var validNumber = false;
+    var rand = Math.floor(Math.random() * 10);
+    var returnNumber = '';
+    while(!validNumber){
+        if(checkForValidNumber(rand,returnNumber)){
+            returnNumber = returnNumber + rand;
+        }
+        else rand = Math.floor(Math.random() * 10);
+    }
+    return returnNumber;
+}
+
+function checkForValidNumber(randToCheck,number){
+    for (let i = 0; i < number.length; i++) {
+        if(randToCheck == number[i]) return false;
+    }
+    return true;
 }
 
 function guess(){
 
     var guessInp = getElementById("guessInput").value;
+
 }
     
